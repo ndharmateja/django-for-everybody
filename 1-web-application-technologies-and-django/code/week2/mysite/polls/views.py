4206c422a719c935d56671f012aac3a1534eb2cd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-08 18:17:07 +0530

    move mysite project into a new repo

--- 1-web-application-technologies-and-django/code/week2/mysite/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-
-# Create your views here.
-def index(_: HttpRequest):
-    return HttpResponse("Hello, world. 7ca7066d is the polls index.")
